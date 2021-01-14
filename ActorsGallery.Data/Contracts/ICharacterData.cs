c3f6fbf8a3a7a7effc53e68d21f23826e2142f00
Philip Newman <philipeano@gmail.com>
2021-01-14 19:11:48 +0100

    Fixes #3 Define data service contracts

---
+++ ActorsGallery.Data/Contracts/ICharacterData.cs
@@ -0,0 +1,12 @@
+﻿using ActorsGallery.Core.Models;
+using System.Collections.Generic;
+
+namespace ActorsGallery.Data.Contracts
+{
+    public interface ICharacterData
+    {
+        public IEnumerable<Character> GetAllCharacters(string sortKey, string sortDirection);
+
+        public IEnumerable<Character> GetCharactersByFilter(string filterKey, string filterValue);
+    }
+}
