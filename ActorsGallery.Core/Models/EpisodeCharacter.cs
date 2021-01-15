14cf03fb2db2fb82cc5ba48e40aea3404fb1121d
Philip Newman <philipeano@gmail.com>
2021-01-15 14:00:08 +0100

    Add new EpisodeCharacter model

---
+++ ActorsGallery.Core/Models/EpisodeCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActorsGallery.Core.Models
+{
+    public class EpisodeCharacter
+    {
+        [Required]
+        public long Id { get; set; }
+
+
+        [Required]
+        public long EpisodeId { get; set; }
+
+
+        [Required]
+        public long CharacterId { get; set; }
+
+
+        [Required]
+        public string RoleName { get; set; }
+
+
+        [Required]
+        public DateTime Created { get; set; }
+
+
+        public Episode Episode { get; set; }
+
+
+        public Character Character { get; set; }
+    }
+}
