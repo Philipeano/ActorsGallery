9b0ae2801b1ff2b7df9aa5c70ff2c2c5ed9562cd
Philip Newman <philipeano@gmail.com>
2021-01-19 12:27:22 +0100

    Implement custom response formatter

---
+++ ActorsGallery.Data/Utilities/Formatter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsGallery.Data.Utilities
+{
+    public class Formatter
+    {
+        public ResponseBody Render(string message, dynamic payload)
+        {
+            ResponseBody response = new ResponseBody
+            {
+                Status = "normal",
+                Payload = payload,
+                Message = message
+            };
+            return response;
+        }
+
+
+        public ResponseBody Render(string errorMessage)
+        {
+            ResponseBody response = new ResponseBody
+            {
+                Status = "error",
+                Payload = null,
+                Message = errorMessage
+            };
+            return response;
+        }
+    }
+}
