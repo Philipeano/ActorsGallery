b29b49d81ac5d2c7747fb4ed48092248b77d9341
Philip Newman <philipeano@gmail.com>
2021-01-19 17:28:13 +0100

    Remove unused namespaces in Characters controller

--- ActorsGallery/Controllers/CharactersController.cs
+++ ActorsGallery/Controllers/CharactersController.cs
@@ -3,10 +3,7 @@ using ActorsGallery.Data.Contracts;
 using ActorsGallery.Data.Utilities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ActorsGallery.Controllers
 {
